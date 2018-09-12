7506e1e9f819f3f5bb82989ccb1314a4f4665f17
mihailsivovna <mihailsivovna@gmail,com>
2018-09-12 20:28:28 +0300

    homework 2

--- Automation.App/Program.cs
+++ Automation.App/Program.cs
@@ -10,7 +10,35 @@ namespace Automation.App
     {
         static void Main(string[] args)
         {
-            
+
+            string name;
+            Console.Write("Enter your name: ");
+            name =Console.ReadLine();
+            string surname;
+            Console.Write("Enter your surname: ");
+            surname = Console.ReadLine();
+            Console.WriteLine("Nice to meet you," + " " + name + " " + surname);
+            Console.ReadLine();
+
+
+
+            float a = 28.97f;
+            int b = 356;
+            float c = a * b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+
+            int d = 59;
+            int e = 7;
+            int f = 59 / 7;
+            int h = 59 % 7;
+            Console.WriteLine(f);
+            Console.WriteLine(h);
+            Console.ReadKey();
+
+
+
         }
 
 
