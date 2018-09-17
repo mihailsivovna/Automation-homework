2f0db699bfc4793042589b0a3f981f3c15fe529c
Михайло Сiвовна <mihailsivovna@gmail.com>
2018-09-17 10:39:14 +0300

    homework

--- Automation.App/Program.cs
+++ Automation.App/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Automation.App
+namespace Automation_App
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Введите год: ");
+            string year = Console.ReadLine();
+            int n = Convert.ToInt32(year);
+            int m;
+            m = (n % 4);
+            if (m == 0)
+                Console.WriteLine($"{year} - год высокосный ");
+            else
+                Console.WriteLine($"{year} - год невысокосный ");
+            Console.ReadLine();
 
 
 
@@ -40,76 +51,32 @@ namespace Automation.App
                 case "Воскресение":
                     Console.WriteLine($"{week} - выходной день недели");
                     break;
-
-               
             }
 
             Console.Write("Введите размер: ");
-            string input = Console.ReadLine();
-            int a = Convert.ToInt32(input);
-            int b = Convert.ToInt32(input);
-            int c = Convert.ToInt32(input);
-            int d = Convert.ToInt32(input);
-            int h = Convert.ToInt32(input);
-            int g = Convert.ToInt32(input);
-            int j = Convert.ToInt32(input);
-            int k = Convert.ToInt32(input);
-
-
+            string size = Console.ReadLine();
+            int a = Convert.ToInt32(size);
 
 
             if (a <= 31)
                 Console.WriteLine("xxs");
-            else if (b > 31 && b <= 32)
+            else if (a <= 32)
                 Console.WriteLine("xs");
-            else if (c > 32  && c <= 36)
+            else if (a <= 36)
                 Console.WriteLine("s");
-            else if (d > 36 && d <= 40)
+            else if (a <= 40)
                 Console.WriteLine("m");
-            else if (h > 40 && h <= 44)
+            else if (a <= 44)
                 Console.WriteLine("l");
-            else if (g > 44 && g <=48)
+            else if (a <= 48)
                 Console.WriteLine("xl");
-            else if (j > 48 && j <= 50)
+            else if (a <= 50)
                 Console.WriteLine("2xl");
-            else if (k > 50)
-                    Console.WriteLine("Неизвестный размер");
-           
+            else if (a > 50)
+                Console.WriteLine("Неизвестный размер");
+
             Console.ReadLine();
-
-
-
-            Console.Write("Введите год: ");
-            string input3 = Console.ReadLine();
-            int m = Convert.ToInt32(input3);
-            int n = Convert.ToInt32(input3);
-            m = (n % 4);
-
-            if (m == 0)
-                Console.WriteLine($"{input3} - год высокосный ");
-            else
-                Console.WriteLine($"{input3} - год невысокосный ");
-            Console.ReadLine();
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
+    }
 }
-}
+
